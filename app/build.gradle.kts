eb7a45aefde57872cd32b47ebbc93dac6361b88f
Monikmedeiros <monikmedeiros@googlemail.com>
2026-03-18 22:55:31 -0300

    configuração das bibliotecas do Firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -59,5 +60,10 @@ dependencies {
     implementation(libs.play.services.location)
     implementation(libs.play.services.maps)
     implementation(libs.play.services.auth)
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.storage.ktx)
 
 }
